1625bee868c0119435f26d532b7a5ab7c3d26ab3
charlotteAuger <auger.cha@gmail.com>
2018-05-09 11:03:38 +0200

    Player prefab, collision spghere for AI

--- Assets/Scripts/IA/AI_Stats.cs
+++ Assets/Scripts/IA/AI_Stats.cs
@@ -3,7 +3,30 @@ using System.Collections.Generic;
 using UnityEngine;
 [CreateAssetMenu (menuName = "PluggableAI/Stats")]
 
-public class AI_Stats : ScriptableObject {
+public class AI_Stats : ScriptableObject 
+{
+	[Header("Attack")]
+	public float attackRadius;
+	public float attackCooldown;
+	public float attackTime;
+	public float offset;
+	public float attackSpeed;
+	public AnimationCurve attackCurve;
+
+	[Header("Chase")]
+	public float chaseSpeed;
+
+	[Header("Patrol")]
+	public float patrolRotationSpeed;
+	public float radius;
+	public float lookRange;
+	public float patrolAngle;
+
+	[Header("Search")]
+	public float searchRotationSpeed;
+	public float searchTime;
+	public float searchAngle;
+
 	public float speed;
 	public float stopDistance;
 }
