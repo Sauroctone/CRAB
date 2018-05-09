e6b39ffefe25730baf32bf0638071c1cfdf8d660
charlotteAuger <auger.cha@gmail.com>
2018-05-09 11:16:14 +0200

    Revert "Player prefab, collision spghere for AI"
    This reverts commit 1625bee868c0119435f26d532b7a5ab7c3d26ab3.

--- Assets/Scripts/IA/AI_Stats.cs
+++ Assets/Scripts/IA/AI_Stats.cs
@@ -3,30 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 [CreateAssetMenu (menuName = "PluggableAI/Stats")]
 
-public class AI_Stats : ScriptableObject 
-{
-	[Header("Attack")]
-	public float attackRadius;
-	public float attackCooldown;
-	public float attackTime;
-	public float offset;
-	public float attackSpeed;
-	public AnimationCurve attackCurve;
-
-	[Header("Chase")]
-	public float chaseSpeed;
-
-	[Header("Patrol")]
-	public float patrolRotationSpeed;
-	public float radius;
-	public float lookRange;
-	public float patrolAngle;
-
-	[Header("Search")]
-	public float searchRotationSpeed;
-	public float searchTime;
-	public float searchAngle;
-
+public class AI_Stats : ScriptableObject {
 	public float speed;
 	public float stopDistance;
 }
